f5661e13de60e0d7d7777d4077978b464e4374d6
Han Ian <ian020805@naver.com>
2026-05-15 02:42:51 +0900

    fix: Trending spot

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,10 +1,14 @@
+import { useSearchParams } from "react-router-dom";
 import BottomNav from "../components/BottomNav";
 import KakaoMap from "../KakaoMap";
 
 export default function MapPage() {
+  const [searchParams] = useSearchParams();
+  const placeId = searchParams.get("placeId");
+
   return (
     <>
-      <KakaoMap />
+      <KakaoMap initialPlaceId={placeId} />
       <BottomNav />
     </>
   );
